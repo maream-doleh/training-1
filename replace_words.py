482cca030ddcba0814b81c0944b9ac0b7b2875ae
maream-doleh <mary.doleh.94@hotmail.com>
2018-05-02 07:19:08 -0400

    modification after review1

--- replace_words.py
+++ replace_words.py
@@ -8,11 +8,36 @@ import argparse
 class WordsModifier:
     default_input_file = 'file1.txt'
 
-    def __init__(self, input_file, output_file, from_list_arr, to_list_arr):
-        self.input_file = input_file
-        self.output_file = output_file
-        self.from_list_arr = from_list_arr
-        self.to_list_arr = to_list_arr
+    def __init__(self, input_file='file1.txt', output_file='file2.txt',
+                 from_list_arr='no,bad', to_list_arr='yes,happy'):
+        self.__input_file = input_file
+        self.__output_file = output_file
+        self.__from_list_arr = from_list_arr
+        self.__to_list_arr = to_list_arr
+
+    def get_input_file(self):
+        return self.__input_file
+
+    def set_input_file(self, input_file):
+        self.__input_file = input_file
+
+    def get_output_file(self):
+        return self.__output_file
+
+    def set_output_file(self, output_file):
+        self.__output_file = output_file
+
+    def get_from_list_arr(self):
+        return self.__from_list_arr
+
+    def set_from_list_arr(self, from_list_arr):
+        self.__from_list_arr = from_list_arr
+
+    def get_to_list_arr(self):
+        return self.__to_list_arr
+
+    def set_to_list_arr(self, to_list_arr):
+        self.__to_list_arr = to_list_arr
 
     def __open_file_to_read_or_write(self, file_rw, opr, text=None):
         if opr == 'r':
@@ -41,40 +66,53 @@ class WordsModifier:
         return None
 
     def replace_words(self):
-        input_text = self.__open_file_to_read_or_write(self.input_file,
+        input_text = self.__open_file_to_read_or_write(self.__input_file,
                                                        opr='r')
         print input_text
-        for old, new in zip(self.from_list_arr, self.to_list_arr):
+        for old, new in zip(self.__from_list_arr, self.__to_list_arr):
             input_text = input_text.replace(old, new)
         print input_text
-        self.__open_file_to_read_or_write(self.output_file,
-                                          opr='w+', text=input_text)
+        return input_text
+
+    def write_to_file(self, replaced_text):
+        self.__open_file_to_read_or_write(self.__output_file,
+                                          opr='w+', text=replaced_text)
+
+
+class ArgParser:
+    arguments = ""
+
+    def __init__(self):
+
+        parser = argparse.ArgumentParser(description='Replace words')
+        parser.add_argument("--input_file", dest='input_file',
+                            help='file contain text you want to modify',
+                            default='file1.txt')
+        parser.add_argument("--output_file", dest='output_file',
+                            help='file where you want to write updated text',
+                            default='file2.txt')
+        parser.add_argument("--from", dest='fromm',
+                            help='list of words you want to replace',
+                            default='no,sad,yesterday')
+        parser.add_argument("--to", dest='to',
+                            help='list of words you want to replace with',
+                            default='yes,happy,today')
+
+        self.arguments = parser.parse_args()
+
+    def get_arguments(self):
+        return self.arguments
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Replace words')
-    parser.add_argument("--input_file", dest='input_file',
-                        help='file contain text you want to modify',
-                        default='file1.txt')
-    parser.add_argument("--output_file", dest='output_file',
-                        help='file where you want to write updated text',
-                        default='file2.txt')
-    parser.add_argument("--from", dest='fromm',
-                        help='list of words you want to replace',
-                        default='no,sad,yesterday')
-    parser.add_argument("--to", dest='to',
-                        help='list of words you want to replace with',
-                        default='yes,happy,today')
-    arguments = parser.parse_args()
+    argParser = ArgParser()
+    argu = argParser.get_arguments()
 
-    input_file = arguments.input_file
-    output_file = arguments.output_file
-    from_list_arr = arguments.fromm.split(',')
-    to_list_arr = arguments.to.split(',')
-    MyWordsModifier = WordsModifier(input_file, output_file,
-                                    from_list_arr, to_list_arr)
+    MyWordsModifier = WordsModifier(argu.input_file, argu.output_file,
+                                    argu.fromm.split(','), argu.to.split(','))
 
-    MyWordsModifier.replace_words()
+    replaced_text = MyWordsModifier.replace_words()
+    MyWordsModifier.write_to_file(replaced_text)
 
 if __name__ == "__main__":
     main()
